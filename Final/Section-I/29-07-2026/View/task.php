a89e132ee4b40a42d861f66675ce45825abb4dc7
hamid-aiub <hamid.uddin@aiub.edu>
2026-07-30 13:53:20 +0600

    updated

---
+++ Final/Section-I/29-07-2026/View/task.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>Hello, This is my first PHP program!</h1>";
+?>
+
+<html>
+    <body>
+        <h1>Hello, This is my first PHP program!</h1>
+    </body>
+</html>
---
+++ Final/Section-Z/29-07-2026/View/task.php
@@ -0,0 +1,15 @@
+<?php
+$name = "John Doe";
+$a = 10;
+$b = 20;
+$sum = $a + $b;
+    echo "<h1>Hello, This is my first PHP program!</h1>";
+    echo "<p>Name: $name</p>";
+    echo "<p>Sum: $sum</p>";
+?>
+
+<html>
+    <body>
+        <h1>Hello, This is my first PHP program!</h1>
+    </body>
+</html>
