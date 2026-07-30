e59e551ce8a9fe0d235c73c4fbcf1c9b501651b4
hamid-aiub <hamid.uddin@aiub.edu>
2026-07-30 16:48:18 +0600

    updated

---
+++ Final/Section-W/View/task.php
@@ -0,0 +1,15 @@
+<?php 
+$a = 10;
+$b = 10;
+$sum = $a + $b;
+echo "The sum of $a and $b is: $sum <br>";
+
+echo "<h1>Hello world</h1>";
+?>
+
+
+<html>
+    <body>
+        <h2>This is my first php program</h2>
+    </body>
+</html>
