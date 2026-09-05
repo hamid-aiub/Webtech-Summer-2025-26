6a12a3914348b6fcb601e952a0cc8e0e750b0eb8
hamid-aiub <hamid.uddin@aiub.edu>
2026-09-05 13:39:46 +0600

    updated ajax

--- Quiz/task.php
+++
@@ -1,9 +0,0 @@
-<html>
-
-<body>
-    <form action="controller.php" method="POST">
-        <button type="submit" name="btn_click"> Perform Action </ button>
-            </ form>
-</body>
-
-</html>
